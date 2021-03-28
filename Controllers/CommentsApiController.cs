0aa66df57d69391c6a04e9e5cd08a8f97a3450ec
bodynar <bodynar@gmail.com>
2021-03-29 00:23:27 +0300

    update api

--- Controllers/CommentsApiController.cs
+++ Controllers/CommentsApiController.cs
@@ -2,58 +2,161 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     using MAS.GitlabComments.Models;
+    using MAS.GitlabComments.Services;
 
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
 
     [ApiController]
     [Route("[controller]")]
-    public class CommentsApiController: ControllerBase
+    public class CommentsApiController : ControllerBase
     {
+        /// <summary>
+        /// Logger to store error information
+        /// </summary>
         private ILogger<CommentsApiController> Logger { get; }
 
-        public CommentsApiController(ILogger<CommentsApiController> logger)
+        /// <summary>
+        /// Service for comments managing
+        /// </summary>
+        private ICommentService CommentService { get; }
+
+        /// <summary>
+        /// Initialize <see cref="CommentsApiController"/>
+        /// </summary>
+        /// <param name="logger">Logger to store error information</param>
+        /// <param name="commentService">Service for comments managing</param>
+        /// <exception cref="ArgumentNullException">Parameter commentService is null</exception>
+        public CommentsApiController(
+            ILogger<CommentsApiController> logger,
+            ICommentService commentService
+        )
         {
             Logger = logger;
+            CommentService = commentService ?? throw new ArgumentNullException(nameof(commentService));
         }
 
-        [HttpGet]
-        public IEnumerable<CommentModel> Get()
-        {
-            return default;
-        }
-
-        [HttpGet]
-        public string GetDescription(Guid commentId)
-        {
-            return default;
-        }
-
-        [HttpPut]
-        public void Add([FromBody]AddCommentModel addCommentModel)
-        {
-
-        }
-
-        [HttpDelete]
-        public void Delete(Guid commentId)
-        {
-
-        }
-
+        /// <summary>
+        /// Add comment by specified values
+        /// </summary>
+        /// <param name="addCommentModel">Comment values</param>
         [HttpPost]
-        public void Update([FromBody]UpdateCommentModel updateCommentModel)
+        public BaseServiceResult Add([FromBody] AddCommentModel addCommentModel)
         {
+            try
+            {
+                CommentService.Add(addCommentModel);
 
+                return BaseServiceResult.Success();
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, "Trying to: add comment.");
+                return BaseServiceResult.Error(e);
+            }
         }
 
-        [HttpPost]
-        public void Increment([FromBody]Guid commentId)
+        /// <summary>
+        /// Get all comments
+        /// </summary>
+        /// <returns>Service perform operation result</returns>
+        [HttpGet]
+        public BaseServiceResult<IEnumerable<CommentModel>> Get()
         {
+            try
+            {
+                var result = CommentService.Get();
 
+                return BaseServiceResult<IEnumerable<CommentModel>>.Success(result);
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, "Trying to: Get all comments");
+                return BaseServiceResult<IEnumerable<CommentModel>>.Error(e);
+            }
+        }
+
+        /// <summary>
+        /// Get description of specified comment
+        /// </summary>
+        /// <param name="commentId">Comment identifier</param>
+        /// <returns>Service perform operation result</returns>
+        [HttpGet]
+        public BaseServiceResult<string> GetDescription(Guid commentId)
+        {
+            try
+            {
+                var result = CommentService.GetDescription(commentId);
+
+                return BaseServiceResult<string>.Success(result);
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, $"Trying to: Get description of comment \"{commentId}\".");
+                return BaseServiceResult<string>.Error(e);
+            }
+        }
+
+        /// <summary>
+        /// Increment <see cref="Comment.AppearanceCount"/> property of specified comment
+        /// </summary>
+        /// <param name="commentId">Comment identifier</param>
+        [HttpPost]
+        public BaseServiceResult Increment([FromBody] Guid commentId)
+        {
+            try
+            {
+                CommentService.Increment(commentId);
+
+                return BaseServiceResult.Success();
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, $"Trying to: Incrementing \"{commentId}\".");
+                return BaseServiceResult.Error(e);
+            }
+        }
+
+        /// <summary>
+        /// Update specified comment by values
+        /// </summary>
+        /// <param name="updateCommentModel">Comment new values</param>
+        [HttpPost]
+        public BaseServiceResult Update([FromBody] UpdateCommentModel updateCommentModel)
+        {
+            try
+            {
+                CommentService.Update(updateCommentModel);
+
+                return BaseServiceResult.Success();
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, $"Trying to: Update comment \"{updateCommentModel?.Id}\".");
+                return BaseServiceResult.Error(e);
+            }
+        }
+
+        /// <summary>
+        /// Delete comments by specified identifiers
+        /// </summary>
+        /// <param name="commentId">Comment identifier</param>
+        [HttpPost]
+        public BaseServiceResult Delete(Guid commentId)
+        {
+            try
+            {
+                CommentService.Delete(commentId);
+
+                return BaseServiceResult.Success();
+            }
+            catch (Exception e)
+            {
+                Logger?.LogError(e, $"Trying to: Delete comment \"{commentId}\".");
+                return BaseServiceResult.Error(e);
+            }
         }
     }
 }
