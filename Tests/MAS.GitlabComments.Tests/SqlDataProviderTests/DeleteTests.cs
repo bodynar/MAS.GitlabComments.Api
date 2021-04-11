cd4c5917d0a11208b2fd3ff1ecae7ceb5c6483df
bodynar <bodynar@gmail.com>
2021-04-11 17:09:00 +0300

    #2 add sql data provider delete tests

---
+++ Tests/MAS.GitlabComments.Tests/SqlDataProviderTests/DeleteTests.cs
@@ -0,0 +1,50 @@
+﻿namespace MAS.GitlabComments.Tests.SqlDataProviderTests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Xunit;
+
+    public sealed class DeleteTests : BaseSqlDataProviderTests
+    {
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenArgumentIsNull()
+        {
+            Guid[] entityIds = null;
+
+            Exception exception =
+                Record.Exception(
+                    () => TestedService.Delete(entityIds)
+                );
+
+            Assert.NotNull(exception);
+            Assert.IsType<ArgumentNullException>(exception);
+        }
+
+        [Fact]
+        public void ShouldNotExecuteCommandWhenEntityIdContainsOnlyDefaultValues()
+        {
+            Guid[] entityIds = new Guid[] { default, default, default };
+
+            TestedService.Delete(entityIds);
+            var lastCommand = LastCommand;
+
+            Assert.Null(lastCommand);
+        }
+
+        [Fact]
+        public void ShouldExecuteCommand()
+        {
+            Guid[] entityIds = new Guid[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+            string expectedSql = $"DELETE FROM [{TestedTableName}] WHERE [Id] IN @P1";
+            IEnumerable<KeyValuePair<string, object>> expectedArguments = new[] { new KeyValuePair<string, object>("P1", entityIds) };
+
+            TestedService.Delete(entityIds);
+            var lastCommand = LastCommand;
+
+            Assert.NotNull(lastCommand);
+            Assert.Equal(expectedSql, lastCommand.Value.Key);
+            AssertArguments(expectedArguments, lastCommand.Value.Value);
+        }
+    }
+}
