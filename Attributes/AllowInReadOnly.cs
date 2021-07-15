f41d2dae166f627383c62b88276ba68632682309
bodynar <bodynar@gmail.com>
2021-07-15 22:23:36 +0300

    add action filter attributes to use read only mode

---
+++ Attributes/AllowInReadOnly.cs
@@ -0,0 +1,9 @@
+﻿namespace MAS.GitlabComments.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class AllowInReadOnlyAttribute : Attribute
+    {
+    }
+}
---
+++ Attributes/UseReadOnlyMode.cs
@@ -0,0 +1,34 @@
+﻿namespace MAS.GitlabComments.Attributes
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    /// <summary>
+    /// Action filter to reject requests for endpoints not marked with <see cref="AllowInReadOnlyAttribute"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class UseReadOnlyModeAttribute : Attribute, IActionFilter
+    {
+        /// <inheritdoc cref="IActionFilter.OnActionExecuted(ActionExecutedContext)"/>
+        public void OnActionExecuted(ActionExecutedContext context) { }
+
+        /// <inheritdoc cref="IActionFilter.OnActionExecuting(ActionExecutingContext)"/>
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionDescriptor is ControllerActionDescriptor controllerDescriptor)
+            {
+                var actionAttributes = controllerDescriptor.MethodInfo.GetCustomAttributes(typeof(AllowInReadOnlyAttribute), false);
+
+                if (actionAttributes == null || !actionAttributes.Any())
+                {
+                    context.Result = new EmptyResult();
+                    return;
+                }
+            }
+        }
+    }
+}
