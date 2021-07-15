68fe1c2bbded6358b6f22d2123e3fa873816371b
bodynar <bodynar@gmail.com>
2021-07-15 22:22:16 +0300

    add app settings model

---
+++ Models/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace MAS.GitlabComments.Models
+{
+    /// <summary>
+    /// Global application settings
+    /// </summary>
+    public class AppSettings
+    {
+        /// <summary>
+        /// Is application in read only mode
+        /// </summary>
+        public virtual bool ReadOnlyMode { get; } // virtual required for unit-tests with Moq
+
+        /// <summary>
+        /// Initializing <see cref="AppSettings"/>
+        /// </summary>
+        /// <param name="readOnlyMode">Is application in read only mode</param>
+        public AppSettings(bool readOnlyMode)
+        {
+            ReadOnlyMode = readOnlyMode;
+        }
+    }
+}
