5819901d72f295f956f5febf3999dfa810d2a57d
bodynar <bodynar@gmail.com>
2021-07-15 22:23:56 +0300

    add app controller tests

---
+++ Tests/MAS.GitlabComments.Tests/AppApiControllerTests/IsReadOnlyTests.cs
@@ -0,0 +1,27 @@
+﻿namespace MAS.GitlabComments.Tests.AppApiControllerTests
+{
+    using Xunit;
+
+    public sealed class IsReadOnlyTests : BaseAppApiControllerTests
+    {
+        [Fact]
+        public void ShouldReturnFalseWhenReadOnlyModeIsFalse()
+        {
+            SettingReadOnlyMode = false;
+
+            var result = TestedController.IsReadOnly();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenReadOnlyModeIsTrue()
+        {
+            SettingReadOnlyMode = true;
+
+            var result = TestedController.IsReadOnly();
+
+            Assert.True(result);
+        }
+    }
+}
