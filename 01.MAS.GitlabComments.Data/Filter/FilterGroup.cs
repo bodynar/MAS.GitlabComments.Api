9996518114cfa6cec8dbaf9eac635fb043007ca6
bodynar <bodynar@gmail.com>
2021-12-13 01:39:46 +0300

    #12 add filter models and utils

---
+++ 01.MAS.GitlabComments.Data/Filter/FilterGroup.cs
@@ -0,0 +1,37 @@
+﻿namespace MAS.GitlabComments.Data.Filter
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Filter group.
+    /// Represents SQL filter conditions
+    /// </summary>
+    public class FilterGroup
+    {
+        /// <summary>
+        /// Unique name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <inheritdoc cref="FilterJoinType"/>
+        public FilterJoinType LogicalJoinType { get; set; }
+
+        /// <summary>
+        /// Filter items
+        /// </summary>
+        public IEnumerable<FilterItem> Items { get; set; } = Enumerable.Empty<FilterItem>();
+
+        /// <summary>
+        /// Nested filter groups
+        /// </summary>
+        public IEnumerable<FilterGroup> NestedGroups { get; set; } = Enumerable.Empty<FilterGroup>();
+
+        /// <summary>
+        /// Is current group contains any filters or nested groups
+        /// </summary>
+        public bool IsEmpty
+            => (Items == null && NestedGroups == null)
+            || !(Items.Any() || NestedGroups.Any()); // props are both null or empty
+    }
+}
---
+++ 01.MAS.GitlabComments.Data/Filter/FilterUtils.cs
@@ -0,0 +1,37 @@
+﻿namespace MAS.GitlabComments.Data.Filter
+{
+    using System;
+
+    using MAS.GitlabComments.Data.Attributes;
+
+    /// <summary>
+    /// Utilities for filter models
+    /// </summary>
+    public static class FilterUtils
+    {
+        /// <summary>
+        /// Get enum SqlOperator attribute value
+        /// </summary>
+        /// <param name="value">Enumeration value</param>
+        /// <returns>Sql operator from attribute if enumeration value has specified attribute; otherwise <see cref="string.Empty"/></returns>
+        public static string GetSqlOperator(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            if (field == null)
+            {
+                return string.Empty;
+            }
+
+            var comparisonOperatorAttributes =
+                field.GetCustomAttributes(typeof(SqlOperatorAttribute), false) as SqlOperatorAttribute[];
+
+            if (comparisonOperatorAttributes != null && comparisonOperatorAttributes.Length > 0)
+            {
+                return comparisonOperatorAttributes[0].Operator;
+            }
+
+            return string.Empty;
+        }
+    }
+}
