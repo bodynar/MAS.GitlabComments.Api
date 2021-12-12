de4b10d2c8e832f7165c0a2e526cebaa56370582
bodynar <bodynar@gmail.com>
2021-12-13 02:53:49 +0300

    #12 add ms filter builder tests

---
+++ Tests/01.MAS.GitlabComments.Data.Tests/MsSqlFilterBuilderTests/BaseMsSqlFilterBuilderTests.cs
@@ -0,0 +1,28 @@
+﻿namespace MAS.GitlabComments.Data.Tests.MsSqlFilterBuilderTests
+{
+    using System;
+
+    using MAS.GitlabComments.Data.Services;
+
+    /// <summary>
+    /// Base class for <see cref="MsSqlFilterBuilder"/> tests
+    /// </summary>
+    public abstract class BaseMsSqlFilterBuilderTests
+    {
+        /// <summary>
+        /// Instance of <see cref="MsSqlFilterBuilder"/> for tests
+        /// </summary>
+        protected MsSqlFilterBuilder TestedService { get; }
+
+        /// <summary> New line </summary>
+        protected static string nl = Environment.NewLine;
+
+        /// <summary>
+        /// Initializing <see cref="BaseSqlDataProviderTests"/> with setup'n all required environment
+        /// </summary>
+        protected BaseMsSqlFilterBuilderTests()
+        {
+            TestedService = new MsSqlFilterBuilder();
+        }
+    }
+}
