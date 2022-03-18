9667c62a675dd14672d38033828c0eea23a9b967
bodynar <bodynar@gmail.com>
2022-03-19 02:40:12 +0300

    #12 update CompleColumnQueryBuilder API

--- 01.MAS.GitlabComments.Data/Services/Contracts/ComplexColumnQueryBuilder.cs
+++ 01.MAS.GitlabComments.Data/Services/Contracts/ComplexColumnQueryBuilder.cs
@@ -11,7 +11,8 @@
         /// Build complex column data to properly select columns from related tables
         /// </summary>
         /// <typeparam name="TProjection">Type of model where to select data</typeparam>
+        /// <param name="sourceTableName">Name of source table</param>
         /// <returns>Instance of <see cref="ComplexColumnData"/> if data built properly, otherwise <see langword="null"/></returns>
-        ComplexColumnData BuildComplexColumns<TProjection>();
+        ComplexColumnData BuildComplexColumns<TProjection>(string sourceTableName);
     }
 }
