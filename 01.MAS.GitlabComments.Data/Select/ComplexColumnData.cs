8e465df5168d80f90c2e791d998a8e91698366b3
bodynar <bodynar@gmail.com>
2022-04-07 23:39:03 +0300

    #12 init properties by default

--- 01.MAS.GitlabComments.Data/Select/ComplexColumnData.cs
+++ 01.MAS.GitlabComments.Data/Select/ComplexColumnData.cs
@@ -1,6 +1,7 @@
 ﻿namespace MAS.GitlabComments.Data.Select
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     /// <summary>
     /// Information about complex columns in select query
@@ -13,11 +14,11 @@
         /// <summary>
         /// Columns paths to include in select
         /// </summary>
-        public IEnumerable<ComplexColumn> Columns { get; set; }
+        public IEnumerable<ComplexColumn> Columns { get; set; } = Enumerable.Empty<ComplexColumn>();
 
         /// <summary>
         /// Tables join data
         /// </summary>
-        public IEnumerable<TableJoinData> Joins { get; set; }
+        public IEnumerable<TableJoinData> Joins { get; set; } = Enumerable.Empty<TableJoinData>();
     }
 }
