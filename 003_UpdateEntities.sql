188e1d4504f5bf30690df706c1552b2c75e85d39
bodynar <bodynar@gmail.com>
2023-09-08 02:17:49 +0300

    add #003 script for updating entities

---
+++ 003_UpdateEntities.sql
@@ -0,0 +1,29 @@
+﻿----
+-- Add base columns into StoryRecords
+----
+ALTER TABLE [dbo].[StoryRecords]
+	ADD [ModifiedOn] [datetime2](7) NULL
+;
+GO
+
+
+----
+-- Add new columns into Comments
+----
+ALTER TABLE [dbo].[Comments]
+	ADD [CommentWithLinkToRule] [nvarchar](max) NOT NULL
+		CONSTRAINT [DF_Comments_Link] DEFAULT ('')
+;
+GO
+
+ALTER TABLE [dbo].[Comments]
+	DROP CONSTRAINT [DF_Comments_Link]
+;
+GO
+
+INSERT INTO [ScriptLog]
+	([CreatedOn], [ScriptName])
+VALUES
+	(GETDATE(), '003_UpdateEntities')
+;
+GO
