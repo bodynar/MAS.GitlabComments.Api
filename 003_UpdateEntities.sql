749cfd2747b6db3f7d3b066c5b1b6c373979388d
bodynar <bodynar@gmail.com>
2023-09-17 01:14:53 +0300

    add before-checks to scripts

--- 003_UpdateEntities.sql
+++ 003_UpdateEntities.sql
@@ -1,29 +1,30 @@
-﻿----
--- Add base columns into StoryRecords
-----
-ALTER TABLE [dbo].[StoryRecords]
-	ADD [ModifiedOn] [datetime2](7) NULL
-;
-GO
+﻿IF NOT EXISTS (
+	SELECT * FROM [ScriptLog]
+	WHERE [ScriptName] = '003_UpdateEntities'
+)
+BEGIN
+    ----
+	-- Add base columns into StoryRecords
+	----
+	ALTER TABLE [dbo].[StoryRecords]
+		ADD [ModifiedOn] [datetime2](7) NULL
+	;
 
+	----
+	-- Add new columns into Comments
+	----
+	ALTER TABLE [dbo].[Comments]
+		ADD [CommentWithLinkToRule] [nvarchar](max) NOT NULL
+			CONSTRAINT [DF_Comments_Link] DEFAULT ('')
+	;
 
-----
--- Add new columns into Comments
-----
-ALTER TABLE [dbo].[Comments]
-	ADD [CommentWithLinkToRule] [nvarchar](max) NOT NULL
-		CONSTRAINT [DF_Comments_Link] DEFAULT ('')
-;
-GO
+	ALTER TABLE [dbo].[Comments]
+		DROP CONSTRAINT [DF_Comments_Link]
+	;
 
-ALTER TABLE [dbo].[Comments]
-	DROP CONSTRAINT [DF_Comments_Link]
-;
-GO
-
-INSERT INTO [ScriptLog]
-	([CreatedOn], [ScriptName])
-VALUES
-	(GETDATE(), '003_UpdateEntities')
-;
-GO
+	INSERT INTO [ScriptLog]
+		([CreatedOn], [ScriptName])
+	VALUES
+		(GETDATE(), '003_UpdateEntities')
+	;
+END
--- SqlScripts/001_Init.sql
+++ SqlScripts/001_Init.sql
@@ -1,55 +1,48 @@
----------
---- Add comments table with constraints
----------
-CREATE TABLE [dbo].[Comments]
-(
-	[Id] [uniqueidentifier] NOT NULL,
-	[CreatedOn] [datetime2](7) NOT NULL,
-	[ModifiedOn] [datetime2](7) NULL,
-	[Message] [nvarchar](max) NOT NULL,
-	[Description] [nvarchar](max) NULL,
-	[AppearanceCount] [int] NOT NULL,
+IF NOT EXISTS (
+	SELECT NULL FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ScriptLog]')
 )
-;
-GO
+BEGIN
+	---------
+	--- Add comments table with constraints
+	---------
+	CREATE TABLE [dbo].[Comments]
+	(
+		[Id] [uniqueidentifier] NOT NULL
+			CONSTRAINT [PK_Comments]
+				PRIMARY KEY CLUSTERED
+			CONSTRAINT [DF_Comments_Id]
+				DEFAULT (NEWID())
+		,
+		[CreatedOn] [datetime2](7) NOT NULL,
+		[ModifiedOn] [datetime2](7) NULL,
+		[Message] [nvarchar](max) NOT NULL,
+		[Description] [nvarchar](max) NULL,
+		[AppearanceCount] [int] NOT NULL
+			CONSTRAINT [DF_Comments_AppearanceCount]
+				DEFAULT ((1))
+		,
+	)
+	;
 
-ALTER TABLE [dbo].[Comments]
-	ADD CONSTRAINT [PK_Comments]
-	PRIMARY KEY CLUSTERED ([Id] ASC)
-;
-GO
-
-ALTER TABLE [dbo].[Comments]
-	ADD CONSTRAINT [DF_Comments_Id]
-	DEFAULT (NEWID()) FOR [Id]
-;
-GO
-
-ALTER TABLE [dbo].[Comments]
-	ADD CONSTRAINT [DF_Comments_AppearanceCount]
-	DEFAULT ((1)) FOR [AppearanceCount]
-;
-GO
+	---------
+	--- Add script log to track version of db
+	---------
+	CREATE TABLE [dbo].[ScriptLog]
+	(
+		[Id] UNIQUEIDENTIFIER NOT NULL
+			CONSTRAINT [DF_ScriptLog_Id]
+				DEFAULT NEWID()
+		,
+		[CreatedOn] DATETIME2(7) NOT NULL,
+		[ScriptName] NVARCHAR(255) NOT NULL
+	)
+	;
 
 
----------
---- Add script log to track version of db
----------
-CREATE TABLE [dbo].[ScriptLog]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL,
-	[CreatedOn] DATETIME2(7) NOT NULL,
-	[ScriptName] NVARCHAR(255) NOT NULL
-)
-;
-
-ALTER TABLE [dbo].[ScriptLog]
-	ADD CONSTRAINT [DF_ScriptLog_Id]
-	DEFAULT NEWID() FOR [Id]
-;
-GO
-
-INSERT INTO [ScriptLog]
-	([CreatedOn], [ScriptName])
-VALUES
-	(GETDATE(), '001_Init')
+	INSERT INTO [ScriptLog]
+		([CreatedOn], [ScriptName])
+	VALUES
+		(GETDATE(), '001_Init')
+	;
+END
