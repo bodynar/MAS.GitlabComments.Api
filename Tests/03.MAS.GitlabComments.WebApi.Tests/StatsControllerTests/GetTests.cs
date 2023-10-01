dfa7faf17c1efd05d6af296de5dffd1b6cd4a6ed
bodynar <bodynar@gmail.com>
2023-10-01 23:26:56 +0300

    add stats api controller tests

---
+++ Tests/03.MAS.GitlabComments.WebApi.Tests/StatsControllerTests/GetTests.cs
@@ -0,0 +1,31 @@
+﻿namespace MAS.GitlabComments.WebApi.Tests.StatsControllerTests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MAS.GitlabComments.Logic.Models;
+    using MAS.GitlabComments.WebApi.Models;
+
+    using Moq;
+
+    using Xunit;
+
+    public sealed class GetTests : BaseStatsControllerTests
+    {
+        [Fact]
+        public void ShouldReturnBaseServiceResultSuccess()
+        {
+            BaseServiceResult<IEnumerable<StoryRecordViewModel>> result = TestedController.Get(It.IsAny<DateTime>(), It.IsAny<DateTime>());
+
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.Empty(result.ErrorMessage);
+        }
+
+        [Fact]
+        public void ShouldReturnBaseServiceResultError()
+        {
+            AssertBaseServiceResultError(() => TestedController.Get(It.IsAny<DateTime>(), It.IsAny<DateTime>()));
+        }
+    }
+}
