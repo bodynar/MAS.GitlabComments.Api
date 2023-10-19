6c6f48c3401048549cd303e3776f8cdd60a5153c
bodynar <bodynar@gmail.com>
2023-10-19 11:46:36 +0300

    add sql #004 with implementing number column for comments

---
+++ 004_AddNumber.sql
@@ -0,0 +1,51 @@
+﻿IF NOT EXISTS (
+	SELECT * FROM [ScriptLog]
+	WHERE [ScriptName] = '004_AddNumber'
+)
+BEGIN
+	----
+	-- Add new columns into Comments
+	----
+
+	ALTER TABLE [dbo].[Comments]
+		ADD [Number] [nvarchar](max) NOT NULL
+			CONSTRAINT [DF_Comments_Number] DEFAULT ('')
+	;
+
+	ALTER TABLE [dbo].[Comments]
+		DROP CONSTRAINT [DF_Comments_Number]
+	;
+
+	---------
+	--- Add SystemVariables table to store system parameters
+	---------
+	CREATE TABLE [dbo].[SystemVariables]
+	(
+		[Id] [uniqueidentifier] NOT NULL
+			CONSTRAINT [DF_SystemVariables_Id]
+				DEFAULT (NEWID())
+			CONSTRAINT [PK_SystemVariables]
+			PRIMARY KEY CLUSTERED ([Id] ASC)
+		,
+		[CreatedOn] [datetime2](7) NOT NULL,
+		[ModifiedOn] [datetime2](7) NULL,
+
+		[Code] [nvarchar](max) NOT NULL,
+		[Caption] [nvarchar](max) NULL,
+		[Type] [nvarchar](max) NOT NULL,
+		[RawValue] [nvarchar](max) NOT NULL,
+	)
+	;
+
+	INSERT INTO [SystemVariables]
+		([CreatedOn], [Code], [Type], [RawValue])
+	VALUES
+		(GETDATE(), 'LastCommentNumber', 'Int', '0')
+	;
+
+	INSERT INTO [ScriptLog]
+		([CreatedOn], [ScriptName])
+	VALUES
+		(GETDATE(), '004_AddNumber')
+	;
+END
