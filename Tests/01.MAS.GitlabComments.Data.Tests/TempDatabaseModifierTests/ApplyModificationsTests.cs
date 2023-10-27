8ae662eeb84ee6108651bcf6dd84e6de451e0f03
bodynar <bodynar@gmail.com>
2023-10-27 03:27:23 +0300

    add tests for temp database modifier service

---
+++ Tests/01.MAS.GitlabComments.Data.Tests/TempDatabaseModifierTests/ApplyModificationsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MAS.GitlabComments.DataAccess.Tests.TempDatabaseModifierTests
+{
+    using Xunit;
+
+    public sealed class ApplyModificationsTests : BaseTempDatabaseModifierTests
+    {
+        [Fact]
+        public void ShouldCreateAndExecuteCommand()
+        {
+            string expectedCommandText = "ALTER TABLE [dbo].[Comments] ADD CONSTRAINT [UQ_Comments_Number] UNIQUE ([Number]);";
+
+            TestedService.ApplyModifications();
+
+            Assert.True(NonQueryExecuted);
+            Assert.Equal(expectedCommandText, CommandText);
+        }
+    }
+}
---
+++ Tests/01.MAS.GitlabComments.Data.Tests/TempDatabaseModifierTests/BaseTempDatabaseModifierTests.cs
@@ -0,0 +1,68 @@
+﻿namespace MAS.GitlabComments.DataAccess.Tests.TempDatabaseModifierTests
+{
+    using System.Data;
+
+    using MAS.GitlabComments.DataAccess.Services;
+    using MAS.GitlabComments.DataAccess.Services.Implementations;
+
+    using Moq;
+
+    public abstract class BaseTempDatabaseModifierTests
+    {
+        /// <summary>
+        /// Instance of <see cref="TempDatabaseModifier"/> for tests
+        /// </summary>
+        protected TempDatabaseModifier TestedService { get; }
+
+        /// <summary>
+        /// Is sql command was executed
+        /// </summary>
+        protected bool NonQueryExecuted { get; private set; }
+
+        /// <summary>
+        /// Value, saved in command sql text
+        /// </summary>
+        protected string CommandText { get; private set; }
+
+        /// <summary>
+        /// Initializing <see cref="BaseTempDatabaseModifierTests"/> with setting up test context
+        /// </summary>
+        protected BaseTempDatabaseModifierTests()
+        {
+            var connectionFactory = GetDbConnectionFactory();
+
+            TestedService = new TempDatabaseModifier(connectionFactory);
+        }
+
+        /// <summary>
+        /// Get mock of <see cref="IDbConnectionFactory"/>, required by tested service
+        /// </summary>
+        /// <returns>Instance of <see cref="IDbConnectionFactory"/></returns>
+        private IDbConnectionFactory GetDbConnectionFactory()
+        {
+            var mock = new Mock<IDbConnectionFactory>();
+
+            var connectionMock = new Mock<IDbConnection>();
+            var commandMock = new Mock<IDbCommand>();
+
+            commandMock
+                .SetupSet(x => x.CommandText = It.IsAny<string>())
+                .Callback<string>(sql => CommandText = sql);
+
+            commandMock
+                .Setup(x => x.ExecuteNonQuery())
+                .Callback(() => NonQueryExecuted = true)
+                .Returns(0);
+
+            connectionMock
+                .Setup(x => x.CreateCommand())
+                .Returns(commandMock.Object);
+
+            mock
+                .Setup(x => x.CreateDbConnection())
+                .Returns(connectionMock.Object);
+
+            return mock.Object;
+        }
+    }
+}
